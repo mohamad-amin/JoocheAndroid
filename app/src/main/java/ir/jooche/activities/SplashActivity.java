a057abd72d7a56ee8b7eb26ce5046e4676c4eb78
mohamad-amin <mohamadamin@aut.ac.ir>
2016-09-08 16:29:31 +0430

    initial commit

---
+++ app/src/main/java/ir/jooche/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package ir.jooche.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.jooche.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
